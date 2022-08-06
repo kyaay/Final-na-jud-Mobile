668f0f5711a22112cd9ef5f24102511d01f65283
Khiara Danielle Rubia <khiara.rubia@usjr.edu.ph>
2022-08-06 21:56:51 +0800

    FINAL

---
+++ bloc_mobile_finals/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'app_router.dart';
+import 'app_themes.dart';
+import 'blocs/bloc_exports.dart';
+import 'screens/tabs_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory());
+
+  HydratedBlocOverrides.runZoned(
+    () => runApp(MyApp(appRouter: AppRouter())),
+    storage: storage,
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key, required this.appRouter}) : super(key: key);
+
+  final AppRouter appRouter;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => TasksBloc()),
+        BlocProvider(create: (context) => SwitchBloc())
+      ],
+      child: BlocBuilder<SwitchBloc, SwitchState>(
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'BloC Tasks App',
+            theme: state.switchValue
+                ? AppThemes.appThemeData[AppTheme.darkMode]
+                : AppThemes.appThemeData[AppTheme.lightMode],
+            home: const TabsScreen(),
+            onGenerateRoute: appRouter.onGenerateRoute,
+          );
+        },
+      ),
+    );
+  }
+}
